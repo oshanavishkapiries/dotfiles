e0405e30af1708f053cf682b5df8df217b906c2a
OSHAN AVISHKA <153918928+oshanavishkapiries@users.noreply.github.com>
2026-07-05 12:02:17 +0530

    feat : style-change-config

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -6,3 +6,42 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+-- Force every remaining UI surface transparent (covers what tokyonight's
+-- `transparent` option and `styles.floats/sidebars` don't already clear)
+local function clear_bg()
+  local groups = {
+    "Normal",
+    "NormalNC",
+    "NormalFloat",
+    "FloatBorder",
+    "FloatTitle",
+    "SignColumn",
+    "LineNr",
+    "CursorLineNr",
+    "EndOfBuffer",
+    "WinSeparator",
+    "VertSplit",
+    "Pmenu",
+    "PmenuSel",
+    "TabLine",
+    "TabLineFill",
+    "StatusLine",
+    "StatusLineNC",
+    "WhichKeyFloat",
+    "TelescopeNormal",
+    "TelescopeBorder",
+    "NoiceCmdline",
+    "NoiceCmdlinePopup",
+  }
+  for _, group in ipairs(groups) do
+    vim.api.nvim_set_hl(0, group, { bg = "none" })
+  end
+end
+
+vim.api.nvim_create_autocmd("ColorScheme", {
+  group = vim.api.nvim_create_augroup("transparent_bg", { clear = true }),
+  callback = clear_bg,
+})
+
+clear_bg()
--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,3 +1,6 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- Sharp (square) borders on all floating windows instead of rounded
+vim.o.winborder = "single"
